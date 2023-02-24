9d3f5be25496f7eeb268d22b36c164aacdf6d2ba
Balajiw920 <wbalaji2021@gmail.com>
2023-02-24 22:43:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-let yourArray = [1, "two", true, null, 5.5];
-      console.log(yourArray);
+let yourArray;
+// assign your array the correct value in the next line
+// your code here!
+yourArray = ['one', 2, true, undefined, null];
+// code end
+
+module.exports = yourArray
